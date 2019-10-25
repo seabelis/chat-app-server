bc2a0648e904ca971b25aacdacb7f9c52d9c0f31
seabelis <seabelis74@gmail.com>
2019-10-25 11:21:05 +0200

    sends messages

--- stream/router.js
+++ stream/router.js
@@ -1,9 +1,19 @@
 const {Router}=require('express')
+const Chatroom = require('./model')
 const router = new Router()
 router.get('/stream', (request, response) => {
   console.log('got a request on /stream')
   response.status(200)
   response.send('it works')
 })
-
+router.post('/message', async (request,response) => {
+  console.log('got a request on /message' ,request.body)
+  const {message} = request.body
+  const entity = await Chatroom.create({
+    message: message
+    // message
+  })
+  response.status(201)
+  response.send('Thanks for your message.')
+})
 module.exports=router
